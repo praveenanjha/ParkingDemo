f1051ca68925e3350b452bf850195ffe2cca6a4f
nisha <nisha@nisha-pc>
2018-04-13 04:12:10 +0530

    creating Parking Lot

--- src/main/java/com/parking/ParkingLotUtility.java
+++ src/main/java/com/parking/ParkingLotUtility.java
@@ -1,12 +1,27 @@
 package com.parking;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ParkingLotUtility {
 
+	int size=0;
 	ArrayList<Integer> listOfAvailableSlots;
+	Map<String, Car> slotAndCarMap;
+	Map<String, String> RegNumAndSlotMap;
+	Map<String, ArrayList<String>> colorOfCarandListofregistrationNumbersMap;
+
 	public void createParkingLot(String parkinglotcount) {
 		// TODO Auto-generated method stub
+		this.size=Integer.parseInt(parkinglotcount);
+		this.listOfAvailableSlots = new ArrayList<Integer>(){} ;
+        for (int i=1; i<= this.size; i++) {
+        	listOfAvailableSlots.add(i);
+        }
+        this.slotAndCarMap = new HashMap<String, Car>();
+        this.RegNumAndSlotMap = new HashMap<String, String>();
+        this.colorOfCarandListofregistrationNumbersMap = new HashMap<String, ArrayList<String>>();
 		System.out.println("Created parking lot with " + parkinglotcount + " slots");
 
 	}
